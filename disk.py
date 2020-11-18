60f73efce489ba763768784c5dbecfbef307c3d8
eiphy <46335499+eiphy@users.noreply.github.com>
2020-11-18 12:26:32 +0800

    Change some content

--- disk.py
+++ disk.py
@@ -1,13 +1,35 @@
 from pathlib import Path
 
+import util as U
+
 class Disk():
     def __init__(self, disk_id):
-        '''Create a dictionary contains the ascii table.'''
+        '''Initialize the Disk class.
+        
+        Create and maintain a ascii table as a dictionary.
+        Store the disk_id which will associated a file to this disk. e.g. disk_id=10, then all data will write to file "10.txt" for this disk.
+        '''
 
     def write_to_file(self, s):
-        '''Given a string, write its hexadecimal representation to disk (file).'''
+        '''Given a string, write its hexadecimal representation to disk (file).
+        
+        To be noticed, this should be finished by appending mode istead of writing mode.
 
-    def decode_string(self, s):
+        Returns:
+            (start_pos, end_pos)
+        '''
+
+    def read_file(self, start, end):
+        '''Read from file and return the hexadecimal number.
+        Args:
+            start: Start reading position.
+            end: End reading position.
+
+        Return:
+            A tuple of reading hexadecimal numbers.
+        '''
+    
+    def encode_string(self, s):
         '''Decode a given string to hexadecimal number.
 
         For instance, "start" will be a list of [0x73, 0x74, 0x61, 0x72, 0x74].
---
+++ util.py
@@ -0,0 +1,19 @@
+
+def decode(data):
+    '''Decode a hexadecimal list to string.
+    
+    ReturnsL
+        A string.
+    '''
+
+def encode(s):
+    '''Encode a given string to hexadecimal number.
+
+    For instance, "start" will be a list of [0x73, 0x74, 0x61, 0x72, 0x74].
+
+    Args:
+        s, Input string.
+    
+    Returns:
+        A list of hexadecimal number.
+    '''

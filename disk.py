ebca59006222d278f9ae0b80299b1121fcb7ef31
eiphy <46335499+eiphy@users.noreply.github.com>
2020-11-20 11:18:23 +0800

    Update disk.py

--- disk.py
+++ disk.py
@@ -2,26 +2,27 @@ import random
 from pathlib import Path
 
 import util as U
-import converter as C
+import convert as C
 
 class Disk():
-    def __init__(self, disk_id, disk_type="normal"):
+    def __init__(self, disk_id, capacity=102400):
         '''Initialize the Disk class.
         
         Create and maintain a ascii table as a dictionary.
-        Store the disk_id which will associated a file to this disk. e.g. disk_id=10, then all data will write to file "10.txt" for this disk.
+        Store the disk_id which will associated a file to this disk. e.g. 
+        disk_id=10, then all data will write to file "10.txt" for this disk.
         '''
-        assert disk_type in ["p", "q", "normal"], "Unknown disk type!"
         self.id = disk_id
         self.size = 0
-        self.type = disk_type
+        self.capacity = capacity
 
     def write_to_file(self, data):
         '''Given a list of hexadecimal number, write it disk (file).
         
         The file is defined by disk_id.
-        To be noticed, this should be finished by appending mode istead of writing mode.
-        Update the 
+        To be noticed, this should be finished by appending mode istead of 
+        writing mode.
+        Check and update the size.
 
         Returns:
             (start_pos, end_pos)
@@ -37,8 +38,9 @@ class Disk():
             A tuple of reading hexadecimal numbers.
         '''
 
-    def corrupt_data(self, n):
-        '''Corrupt n bytes in the disk randomly.'''
+    def lost_data(self, n):
+        '''Lost n bytes in the disk randomly.'''
     
     def __repr__(self):
-        return f'Disk {self.id}, type: {self.type}'
+        return f'Disk {self.id}, used: {self.size}, '                          \
+               f'{self.size / self.capacity * 100}%'
